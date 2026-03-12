82470bf77fb2991a180eb26bc3dfc8c340e5c50b
royanxdev <nurmuhammadroyanbirallah@gmail.com>
2026-03-12 21:49:13 +0700

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar";
 import Home from "./pages/home";
 import LastOffer from "./pages/lastetoffer";
 import Transaction from "./pages/transaction";
 import Contact from "./pages/contact";
-import { useState } from "react";
 import Container from "./components/Container";
 
 function App() {
   return (
     <>
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/last-offer" element={<LastOffer />} />
-        <Route path="/transaction" element={<Transaction />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
       <Container>
-        <h1>hallo</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/last-offer" element={<LastOffer />} />
+          <Route path="/transaction" element={<Transaction />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </Container>
     </>
   );
